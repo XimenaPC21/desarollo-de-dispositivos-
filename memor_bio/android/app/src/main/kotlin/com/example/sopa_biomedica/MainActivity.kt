b545a56718b35c9df9f569340092f4608dac5b05
XimenaPC21 <A01643646@tec.mx>
2026-05-09 16:26:58 -0600

    Descripción del proyecto

---
+++ memor_bio/android/app/src/main/kotlin/com/example/sopa_biomedica/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sopa_biomedica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ memor_bio/mi_sopa_letras/android/app/src/main/kotlin/com/example/mi_sopa_letras/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mi_sopa_letras
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
